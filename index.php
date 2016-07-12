d7a0f94e4df9da95bb6ffd30c90cbe4f011f3a91
Krieg Benally <kriegbenally@gmail.com>
2016-07-12 09:58:12 -0600

    Added files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once("lib/headUtils.php");?>
+</head>
+<body>
+
+<div class="container">
+    <h1 class="bounce">HELLO</h1>
+</div>
+
+<div class="trigger">
+    <div class="box">
+        <p>
+            OMG<br>
+            This is so<br>
+            DOPE!!
+        </p>
+
+    </div>
+</div>
+
+
+</body>
+</html>
